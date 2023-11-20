2bf3d4eeb8c6b0a45bb3e554bc345d7d3ba0fde4
Alberto de Miguel <adml932@gmail.com>
2023-11-20 18:51:53 +0000

    x

--- models/marts/core/dim_users.sql
+++ models/marts/core/dim_users.sql
@@ -17,8 +17,8 @@ SELECT
     email,
     phone_number,
     address_id,
-    created_date,
-    created_time_utc,
-    updated_date,
-    updated_time_utc
+    {{ dbt_utils.generate_surrogate_key(['created_date']) }} AS created_date_key,
+    {{ dbt_utils.generate_surrogate_key(['created_time_utc']) }} AS created_time_utc_key,
+    {{ dbt_utils.generate_surrogate_key(['updated_date']) }} AS updated_date_key,
+    {{ dbt_utils.generate_surrogate_key(['updated_time_utc']) }} AS updated_time_utc_key
 FROM stg_users
