24a4b5501321f9f192454cf7f6b83a350514076b
Alberto de Miguel <adml932@gmail.com>
2023-11-20 18:31:14 +0000

    x

--- tests/stg_orders__status_delivered_2.sql
+++ tests/stg_orders__status_delivered_2.sql
@@ -2,4 +2,4 @@ SELECT *
 FROM {{ ref('stg_sql_server_dbo__orders') }} 
 WHERE 
     status = 'delivered' AND delivered_date IS NULL 
-    OR status = 'delivered' AND delivered_time IS NULL
+    OR status = 'delivered' AND delivered_time_utc IS NULL
