04249fddea47dcaa83aba6a458bafb68211e546f
Alberto de Miguel <adml932@gmail.com>
2023-12-04 11:50:14 +0000

    fixed tests

--- tests/stg_orders__order_total.sql
+++ tests/stg_orders__order_total.sql
@@ -1,3 +1,9 @@
-SELECT *
+SELECT
+    order_cost_usd,
+    shipping_cost_usd,
+    order_total_usd,
+    discount_usd
 FROM {{ ref('stg_sql_server_dbo__orders') }}
-WHERE order_cost_usd + shipping_cost_usd != order_total_usd
+JOIN {{ ref('stg_sql_server_dbo__promos') }}
+USING(promo_key)
+WHERE (order_cost_usd + shipping_cost_usd) - discount_usd != order_total_usd
